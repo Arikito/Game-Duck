197254958d5109f5d6ec3aa2e9df881ae9ec2d24
Alexander Parkhomenko <arikito.91@gmail.com>
2017-12-24 22:24:44 +0200

    Final

--- js/main.js
+++ js/main.js
@@ -1,83 +1,148 @@
 ;(function(){
 	"use srtict"
-	var ViewWidth = document.body.clientWidth,
-		minViewWidth = 300,
-		ViewHeight = document.body.clientHeight,
-		duck = document.getElementById('duck'),
-		aaa = document.querySelector('.sun'),
-		ui = document.getElementById('ui');
 	var totalScore = 0,
-		bullets = 3;
-
+		bullets = 3,
+		duckSize = 0;
+	var ViewWidth = document.body.clientWidth,
+		ViewHeight = document.body.clientHeight,
+		// duck = document.getElementById('duck'),
+		aaa = document.querySelector('.sun'),
+		ui = document.getElementById('ui'),
+		nature = document.getElementById('nature'),
+		sky = nature.querySelector('.sky');
 	function pad(num, size) {
 		var s = num+"";
 		while (s.length < size) s = "0" + s;
 		return s;
 	}
-
-	function DuckObject(x,y){
+	function DuckObject(x, y){
 		var that = this;
 		this.id = 'duck';
 		this.x = x;
 		this.y = y;
-		this.spedDX = 1;
-		this.spedDY = 1;
+		var speeds = [-1, 1];
+		this.speedX = speeds[Math.floor(Math.random() * speeds.length)];
+		this.speedY = -1;
 		this.y = this.y;
 		this.file = 'img/duck.svg';
 		this.scale = 0.5;
-		this.fly = function(i){
-			var topPos = document.documentElement.clientHeight;
-			setInterval(function(){
-				// Moving by Y axis
-				if(that.y == (window.innerHeight - 30) && that.spedDY > 0){
-					that.spedDY = -1;
-					console.log(that.objct.style.bottom + 'right');
-				}else if(that.y ==  0 && that.spedDY < 0){
-					that.spedDY = 1;
-				}
-				that.y += that.spedDY;
-				that.objct.style.bottom = that.y +"px";
-				// Moving by X axis
-				if(that.x == (window.innerWidth - 47) && that.spedDX > 0){
-					that.spedDX = -1;
-				}else if(that.x ==  0 && that.spedDX < 0){
-					that.spedDX = 1;
-				}
-				that.x += that.spedDX;
-				that.objct.style.left = that.x +"px";
-			}, 5);
+		this.fly = function(){
+			// Moving by Y axis
+			if(that.y == (window.innerHeight - 150) && that.speedY > 0){
+				that.speedY = -1;
+			}else if(that.y == 0 && that.speedY < 0){
+				that.speedY = 1;
+			}
+			that.y += that.speedY;
+			that.objct.style.top = that.y + 'px';
+			// Moving by X axis
+			if(that.x == (window.innerWidth - duckSize) && that.speedX > 0){
+				that.speedX = -1;
+			}else if(that.x == 0 && that.speedX < 0){
+				that.speedX = 1;
+			}
+			that.x += that.speedX;
+			that.objct.style.left = that.x + 'px';
 		};
-		this.create = function(i){
+		this.flyout = function(){
+			that.speedY = -1.5;
+			that.y += that.speedY;
+			if(that.y > -duckSize){
+				that.y += that.speedY;
+				that.objct.style.top = that.y + 'px';
+			}else{
+				clearInterval(that.move);
+				that.objct.remove();
+				game.init();
+			}
+		};
+		this.die = function(){
+			clearInterval(this.move);
+			setTimeout(function(){
+				that.objct.remove();
+				game.init();
+			}, 1000);
+		}
+		this.create = function(){
 			this.objct.id = this.id;
 			document.body.appendChild(this.objct);
 			this.objct = document.getElementById(this.id);
-			
-			this.objct.onclick = function(e){
-				e.preventDefault();
-				var item = e.target;
-				item.className = "dead";
-				item.style.backgroundColor = "red";
-				totalScore += 500;
-				// console.log(item);
-				ui.querySelector('.score .number').innerText = pad(totalScore, 8);
-				setTimeout(function(){
-					that.die();
-				}, 1000);
-			}
-			this.fly();
+			duckSize = this.objct.clientHeight;
+			this.x = Math.floor(Math.random() * (ViewWidth-duckSize - duckSize) + duckSize);
+			this.y = window.innerHeight-180;
+			this.move = setInterval(this.fly, 10);
 		};
 		if(!(this.objct = document.getElementById(this.id))){
 			this.objct = document.createElement("button");
 			this.create();
 		};
-		this.die = function(){
-			this.objct.remove();
-			clearInterval(this.fly);
+	};
+	function DuckHunt(){
+		var that = this;
+		this.play = false;
+		this.life = 3;
+		this.bullets = ui.querySelector('.shot .bullets').getElementsByClassName('bullet');
+		this.reloadBullets = function(count){
+			ui.querySelector('.shot .bullets').innerHTML = '';
+			for(var i = 0; i < count; i++){
+				var bullet = document.createElement('div');
+				bullet.className = 'bullet';
+				ui.querySelector('.shot .bullets').appendChild(bullet);
+			}
+			this.bullets = ui.querySelector('.shot .bullets').getElementsByClassName('bullet');
+		}
+		this.init = function(){
+			this.play = true;
+			this.reloadBullets(3);
+			ui.querySelector('.shot .lifes .number').innerText = this.life;
+			setTimeout(function(){
+				if(that.life <= 0){
+					game.over();
+				}else{
+					that.duck = new DuckObject();
+					document.body.onmousedown = function(e){
+						e.preventDefault();
+						if(that.play){
+							that.bullets[0].remove();
+							if(e.clientX > that.duck.x && e.clientX < (that.duck.x + duckSize) && e.clientY > that.duck.y && e.clientY < (that.duck.y + duckSize)){
+								that.play = false;
+								totalScore += 500;
+								ui.querySelector('.score .number').innerText = pad(totalScore, 8);
+								that.duck.die();
+							}else{
+								sky.className = 'sky red';
+								setTimeout(function(){
+									sky.className = 'sky';
+								}, 100);
+								if(that.bullets.length == 0){
+									that.play = false;
+									that.life--;
+									ui.querySelector('.shot .lifes .number').innerText = that.life;
+									clearInterval(that.duck.move);
+									that.duck.move = setInterval(that.duck.flyout, 5);
+								}
+							}
+						}
+					}
+				}
+			}, 100);
+		};
+		this.over = function(){
+			this.play = false;
+			gameOver.style.display = 'block';
 		}
 	};
-	create.onclick = function(){
-		var randPointX = Math.floor(Math.random() * (ViewWidth - minViewWidth) + minViewWidth);
-		var duckObj = new DuckObject(randPointX,200);
-	}
+	var game = new DuckHunt();
+	
+	start.onclick = function(){
+		startScreen.style.display = 'none';
+		game.init();
+		// var randPointX = Math.floor(Math.random() * (ViewWidth - minViewWidth) + minViewWidth);
+		// var duckObj = new DuckObject(randPointX,200);
+	};
+
+	gameOver.onclick = function(){
+		location.reload();
+	};
 
 }());
